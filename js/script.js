16aa047cc749ebd96978872a01f5f8688e281b7d
Aleksandr <shur.goog@gmail.com>
2018-10-30 20:27:20 +0200

    Perth first final version
    Done with menu div opacity
    done with PP

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 (function () {
     
-    var el = document.getElementById('menu');
+    var menuOpen = document.getElementById('menu');
     
-    el.onmousedown = function () {
-      var div = document.getElementsByClassName('main-menu'); 
-        div[0].style.display = "block";
+    menuOpen.onmousedown = function () {
+      var show = document.getElementsByClassName('main-menu'); 
+        show[0].style.display = "block";
     };
     
 })();
+
+(function () {
+    
+    var menuClose = document.getElementsByClassName('close');
+    
+    menuClose[0].onmousedown = function () {
+      var closing = document.getElementsByClassName('main-menu'); 
+        closing[0].style.display = "none";
+    };
+    
+})();
+
